ce7ed64d5c160000c7cb5db78c15ea6bbaacc57a
aaa <ayg0508@outlook.com>
2020-06-26 18:56:14 +0900

    Add customer add/remove feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
-import Customer from './components/Customer'
+import Customer from './components/Customer';
+
+import CustomerAdd from './components/CustomerAdd';
 
 import './App.css';
 
@@ -52,7 +54,11 @@ margin: theme.spacing.unit * 2
 
 class App extends Component {
 
-state = {
+constructor(props) {
+
+super(props);
+
+this.state = {
 
 customers: '',
 
@@ -60,7 +66,29 @@ completed: 0
 
 }
 
+this.stateRefresh = this.stateRefresh.bind(this);
 
+}
+
+
+
+stateRefresh() {
+
+this.setState({
+
+customers: '',
+
+completed: 0
+
+});
+
+this.callApi()
+
+.then(res => this.setState({customers: res}))
+
+.catch(err => console.log(err));
+
+}
 
 componentDidMount() {
 
@@ -112,57 +140,36 @@ const { classes } = this.props;
 
 return (
 
+<div>
 <Paper className={classes.root}>
-
 <Table className={classes.table}>
-
 <TableHead>
-
 <TableRow>
-
 <TableCell>번호</TableCell>
-
 <TableCell>이미지</TableCell>
-
 <TableCell>이름</TableCell>
-
 <TableCell>생년월일</TableCell>
-
 <TableCell>성별</TableCell>
-
 <TableCell>직업</TableCell>
-
+<TableCell>설정</TableCell>
 </TableRow>
-
 </TableHead>
-
 <TableBody>
-
 {this.state.customers ?
-
 this.state.customers.map(c => {
-
-return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-
+return <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
 }) :
-
 <TableRow>
-
 <TableCell colSpan="6" align="center">
-
 <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
-
 </TableCell>
-
 </TableRow>
-
 }
-
 </TableBody>
-
 </Table>
-
 </Paper>
+<CustomerAdd stateRefresh={this.stateRefresh} />
+</div>
 
 );
 
