c3aeb7fa92ddfeefcf17316e27e6161dc4053e15
aaa <ayg0508@outlook.com>
2020-06-26 18:07:05 +0900

    Add node.js express server

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/customers', (req, res) => {
+setTimeout(function() {
+res.send([
+{
+'id': 1,
+'image': 'https://placeimg.com/64/64/1',
+'name': '홍길동',
+'birthday': '961222',
+'gender': '남자',
+'job': '대학생'
+},
+{
+'id': 2,
+'image': 'https://placeimg.com/64/64/2',
+'name': '김김김',
+'birthday': '960508',
+'gender': '남자',
+'job': '프로그래머'
+},
+{
+'id': 3,
+'image': 'https://placeimg.com/64/64/3',
+'name': '리리리',
+'birthday': '961127',
+'gender': '남자',
+'job': '디자이너'
+}
+]);
+}, 3000);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
